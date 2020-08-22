1da199b1cf9d8c0478b8aa80d20b70d55e702a8e
Myron Dulay <myrondulay@tuta.io>
2020-08-22 13:02:30 +0800

    Initial Commit

---
+++ Seed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Seed
+//
+//  Created by Myron Dulay on 8/22/20.
+//  Copyright © 2020 Myron Dulay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
