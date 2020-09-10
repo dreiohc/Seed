cca78bfd7f58717ced7e9f0f1c52b887fec6395a
Myron Dulay <myrondulay@tuta.io>
2020-09-10 19:09:43 +0800

    - Add color presets and UIColor Extension

---
+++ Seed/Extensions/UIColor.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor.swift
+//  Seed
+//
+//  Created by Myron Dulay on 9/10/20.
+//  Copyright © 2020 Myron Dulay. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	static let seedCream = UIColor(hex: 0xf7f5dd)
+	static let seedMagenta = UIColor(hex: 0xe7305b)
+	static let seedPaleGreen = UIColor(hex: 0x9bdeac)
+	static let seedSalmon = UIColor(hex: 0xe2979c)
+}
