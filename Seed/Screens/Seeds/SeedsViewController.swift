489cbe92b2448c5cfbbe6ca58b1978add1e032e8
Myron Dulay <myrondulay@tuta.io>
2020-09-10 19:10:54 +0800

    - Add pagination for seed collection view. - Adjust size of seed collection view - Update color of seed collection view - Update constraints and sizes of seed collection view

--- Seed/Screens/Seeds/SeedsViewController.swift
+++ Seed/Screens/Seeds/SeedsViewController.swift
@@ -9,104 +9,77 @@
 import UIKit
 
 class SeedsViewController: UIViewController {
-
+	
 	let data = [
 		SeedData(title: "My First Seed"),
 		SeedData(title: "My Second Seed"),
 		SeedData(title: "My Third Seed"),
 	]
 	
-	
-	
 	fileprivate let collectionView: UICollectionView = {
 		let layout = UICollectionViewFlowLayout()
 		layout.scrollDirection = .horizontal
 		let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
 		cv.translatesAutoresizingMaskIntoConstraints = false
-		cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+		cv.register(SeedViewCell.self, forCellWithReuseIdentifier: "cell")
 		return cv
 	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.setupCollectionView()
+		self.view.backgroundColor = .seedCream
 		
 	}
-
 	
 	private func setupCollectionView() {
 		view.addSubview(collectionView)
-		collectionView.backgroundColor = .white
-		collectionView.showsHorizontalScrollIndicator = false
-		collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-		collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
-		collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
-		collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.5).isActive = true
-		collectionView.delegate = self
-		collectionView.dataSource = self
+		self.collectionView.backgroundColor = .white
+		self.collectionView.showsHorizontalScrollIndicator = false
+		self.collectionView.delegate = self
+		self.collectionView.dataSource = self
+		self.collectionView.isPagingEnabled = true
+		self.collectionView.backgroundColor = .seedCream
+		self.collectionView.layer.cornerRadius = 2.0
+		let screenSize = UIScreen.main.bounds
+		let screenHeight = screenSize.height
+		let screenWidth = screenSize.width
+		NSLayoutConstraint.activate([
+			self.collectionView.widthAnchor.constraint(equalToConstant: (screenWidth * 0.7)),
+			self.collectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+			self.collectionView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+			self.collectionView.heightAnchor.constraint(equalToConstant: (screenHeight * 0.5))
+		])
 	}
-	
 }
 
+
 extension SeedsViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-	
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
-	}
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return data.count
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SeedViewCell
 		cell.data = self.data[indexPath.row]
-		cell.backgroundColor = .black
+		cell.backgroundColor = .seedMagenta
+		cell.layer.cornerRadius = 4.0
 		return cell
 	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return 10
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let frameSize = collectionView.frame.size
+		return CGSize(width: frameSize.width - 10, height: frameSize.height)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+	}
+	
 }
 
-
-class CustomCell: UICollectionViewCell {
-	
-	var data: SeedData? {
-		didSet {
-			guard let data = data else { return }
-			label.text = data.title
-		}
-	}
-	
-	fileprivate let label: UILabel = {
-		let title = UILabel()
-		title.textColor = .white
-		title.numberOfLines = 0
-		title.translatesAutoresizingMaskIntoConstraints = false
-		title.textAlignment = .center
-		return title
-	}()
-	
-	
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		contentView.addSubview(label)
-		label.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-		label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-		label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-		label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-	}
-	
-	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-}
-
-
-
-
-// Uncomment for debug handling
-//extension UIApplication {
-//	var isDebugMode: Bool {
-//		let dictionary = ProcessInfo.processInfo.environment
-//		return dictionary["DEBUGMODE"] != nil
-//	}
-//}
